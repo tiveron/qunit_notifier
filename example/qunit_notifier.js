0d480ba2bca456442f782b65f83a70aa608c76c6
Thiago Tiveron Fávaro <thiagotf9@gmail.com>
2013-01-22 21:04:16 -0200

    adding phantomjs features

--- example/qunit_notifier.js
+++ example/qunit_notifier.js
@@ -3,7 +3,8 @@ var qnotifier = require('../lib/main')
 
 qnotifier.configure({
 	testPath: path.join(__dirname, 'cases'),
-	runner: path.join(__dirname, 'index.html')
+	runner: path.join(__dirname, 'index.html'),
+	notifier: 'growl'
 });
 
 qnotifier.watch();
