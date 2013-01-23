546e88391c322eac3f9570cfb3803ef52e1c1fee
tfavaro <tfavaro@brioco.(none)>
2013-01-23 18:21:00 -0200

    adding libnotify class

--- example/qunit_notifier.js
+++ example/qunit_notifier.js
@@ -1,10 +1,10 @@
-var qnotifier = require('../lib/main')
-	path = require('path');
+var qnotifier = require('../lib/main');
+	var path = require('path');
 
 qnotifier.configure({
 	testPath: path.join(__dirname, 'cases'),
 	runner: path.join(__dirname, 'index.html'),
-	notifier: 'snarl'
+	notifier: 'growl'
 });
 
 qnotifier.watch();
