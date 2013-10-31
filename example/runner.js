bbd67cb59f51689adc20a1474d63bb9d4aa4823b
Thiago Tiveron <thiagotf9@gmail.com>
2013-10-31 20:26:37 -0200

    introducing standalone tests

--- example/runner.js
+++ example/runner.js
@@ -1,11 +1,12 @@
 var path = require('path');
 
 var qunit_notifier = {
-    testPath: path.join(__dirname, 'cases'),
-    runner: path.join(__dirname, 'index.html'),
-    notifier: 'snarl',
-    theme: 'doom'
+    testPath: path.join(__dirname, 'cases')
+    ,runner: path.join(__dirname, 'index.html')
+    ,notifier: 'libnotify'
+    ,theme: 'doom'
     //,silent: true
+    //,watch: false
 };
 
 module.exports = qunit_notifier;
